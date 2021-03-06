d0eda148467ff7d7a60972aceb4c0a0c49d4a7ff
Wannes <wanneske_v@hotmail.com>
2021-03-06 14:34:26 +0100

    Toggle content and basic Image fallback

--- src/views/Files.vue
+++ src/views/Files.vue
@@ -5,41 +5,46 @@
             <router-link :to="{name: 'Commits', params: {name: route.params.name}}">{{ route.params.name }}</router-link> /
             Files
         </div>
-        <div class="files" v-for="file in files" :key="file.sha">
-            <div class="files__header">
-                <div class="files__header__name">{{ file.filename }}</div>
-                <div class="files__header__changes">
-                    <span v-if="file.additions" class="addition">+ {{ file.additions }}</span>
-                    <span v-if="file.deletions" class="deletions">- {{ file.deletions }}</span>
+        <Loader v-if="loadingData && !error"/>
+        <div v-if="!loadingData && !error">
+            <div class="files" v-for="file in files" :key="file.sha">
+                <div class="files__header" @click="toggleContent">
+                    <div class="files__header__name">{{ file.filename }}</div>
+                    <div class="files__header__changes">
+                        <span v-if="file.additions" class="addition">+ {{ file.additions }}</span>
+                        <span v-if="file.deletions" class="deletions">- {{ file.deletions }}</span>
+                    </div>
+                </div>
+                <div class="files__content">
+                    <ssh-pre language="html" v-if="file.filename.includes('.html')">
+                        {{ file.patch }}
+                    </ssh-pre>
+                    <ssh-pre language="js" v-else-if="file.filename.includes('.js')">
+                        {{ file.patch }}
+                    </ssh-pre>
+                    <ssh-pre language="php" v-else-if="file.filename.includes('.php')">
+                        {{ file.patch }}
+                    </ssh-pre>
+                    <ssh-pre language="json" v-else-if="file.filename.includes('.json')">
+                        {{ file.patch }}
+                    </ssh-pre>
+                    <ssh-pre language="xml" v-else-if="file.filename.includes('.xml')">
+                        {{ file.patch }}
+                    </ssh-pre>
+                    <ssh-pre language="pug" v-else-if="file.filename.includes('.pug')">
+                        {{ file.patch }}
+                    </ssh-pre>
+                    <ssh-pre language="css" v-else-if="file.filename.includes('.css')">
+                        {{ file.patch }}
+                    </ssh-pre>
+                    <img :src="file.raw_url" v-else-if="file.filename.includes('.jpg') || file.filename.includes('.jpeg') || file.filename.includes('.png') || file.filename.includes('.gif')"/>
+                    <ssh-pre v-else>
+                        {{ file.patch }}
+                    </ssh-pre>
                 </div>
             </div>
-            <div class="files__content">
-                <ssh-pre language="html" v-if="file.filename.includes('.html')">
-                    {{ file.patch }}
-                </ssh-pre>
-                <ssh-pre language="js" v-else-if="file.filename.includes('.js')">
-                    {{ file.patch }}
-                </ssh-pre>
-                <ssh-pre language="php" v-else-if="file.filename.includes('.php')">
-                    {{ file.patch }}
-                </ssh-pre>
-                <ssh-pre language="json" v-else-if="file.filename.includes('.json')">
-                    {{ file.patch }}
-                </ssh-pre>
-                <ssh-pre language="xml" v-else-if="file.filename.includes('.xml')">
-                    {{ file.patch }}
-                </ssh-pre>
-                <ssh-pre language="pug" v-else-if="file.filename.includes('.pug')">
-                    {{ file.patch }}
-                </ssh-pre>
-                <ssh-pre language="css" v-else-if="file.filename.includes('.css')">
-                    {{ file.patch }}
-                </ssh-pre>
-                <ssh-pre v-else>
-                    {{ file.patch }}
-                </ssh-pre>
-            </div>
         </div>
+        <ErrorMessage v-if="error" :errorMsg="error.message"/>
     </div>
 </template>
 
@@ -47,36 +52,53 @@
 import { ref } from '@vue/reactivity';
 import { useRoute } from 'vue-router';
 
+/* https://antoniandre.github.io/simple-syntax-highlighter/ */
 import SshPre from 'simple-syntax-highlighter';
 import 'simple-syntax-highlighter/dist/sshpre.css';
 
 import { apiCall } from '../composables/GlobalFunctions';
+import Loader from "../components/Loader"
+import ErrorMessage from "../components/ErrorMessage"
 
 export default {
     components:{
-        SshPre
+        SshPre,
+        Loader,
+        ErrorMessage
     },
     setup(){
         const route = useRoute();
         const files = ref([]);
-
-        console.log(route.params);
+        const loadingData = ref(true);
+        const error = ref("");
 
         const getDetails = async () => {
             const {data, errorMsg, loadData} = apiCall(`/repos/${route.params.username}/${route.params.name}/commits/${route.params.id}`);
             await loadData();
 
-            files.value = data.value.files;
-
-            console.log(files.value,errorMsg);
+            if(errorMsg.value){
+                error.value = errorMsg;
+            } else {
+                files.value = data.value.files;
+                loadingData.value = false;
+            }
         }
 
         getDetails();
 
+        // hide/show content of files
+        const toggleContent = (e) => {
+            e.currentTarget.nextSibling.classList.toggle("hidden");
+            e.currentTarget.classList.toggle("toggled");
+        }
+
         return{
+            loadingData,
+            error,
             route,
             files,
             getDetails,
+            toggleContent
         }
     }
 }
@@ -88,6 +110,8 @@ export default {
         margin-bottom: 15px;
     }
     .files{
+        margin-bottom: 30px;
+
         &__header{
             display: flex;
             justify-content: space-between;
@@ -98,9 +122,35 @@ export default {
             border-top-right-radius: 6px;
             padding: 15px;
             text-align: left;
+            cursor: pointer;
 
             &__name{
                 font-weight: bold;
+
+                &:before{
+                    content: "";
+                    display: inline-block;
+                    width: 0;
+                    height: 0;
+                    vertical-align: middle;
+                    border-top: 4px solid;
+                    border-right: 4px solid transparent;
+                    border-bottom: 0 solid transparent;
+                    border-left: 4px solid transparent;
+                    color: lightgray;
+                    margin-right: 10px;
+                }
+            }
+
+            &.toggled{
+                .files__header__name{
+                    &:before{
+                        border-top: 0 solid transparent;
+                        border-right: 4px solid transparent;
+                        border-bottom: 4px solid;
+                        border-left: 4px solid transparent;
+                    }
+                }
             }
 
             &__changes{
@@ -124,6 +174,19 @@ export default {
             border-bottom-left-radius: 6px;
             border-bottom-right-radius: 6px;
             text-align: left;
+            max-height: 300px;
+            overflow-y: scroll;
+
+            transition: max-height .75s ease-out;
+
+            &.hidden{
+                max-height: 0;
+            }
+
+            img{
+                max-width: 100%;
+                padding: 15px 0;
+            }
 
             .ssh-pre{
                 border: none;
